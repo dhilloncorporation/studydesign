b9166da13e806c12e5cc6265758d3ecac697ec5e
dhilloncorporation <154770480+dhilloncorporation@users.noreply.github.com>
2025-02-08 12:12:51 +1100

    Delete studydesign directory

--- studydesign/src/main/java/com/dhilloncorp/studydesign/repository/StudyDesignRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dhilloncorp.studydesign.repository;
-
-import com.dhilloncorp.studydesign.model.StudyDesign;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyDesignRepository extends JpaRepository<StudyDesign, Long> {
-}
